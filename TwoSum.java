81aac977617ad665c61bee201946764c4823ac6b
JAGADEESH17-H <jagadeesh020917@gmail.com>
2026-04-19 14:58:49 +0530

    Add Two Sum solution using brute force approach

---
+++ TwoSum.java
@@ -0,0 +1,21 @@
+package com;
+
+public class TwoSum {
+
+	public int[] twoSum(int[] nums, int target) {
+
+		int[] rs = new int[2];
+
+		outer: for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					rs[0] = i;
+					rs[1] = j;
+					break outer;
+				}
+			}
+		}
+
+		return rs;
+	}
+}
