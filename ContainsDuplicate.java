563d5eac14686931835233584c3f6ed89c68a041
JAGADEESH17-H <jagadeesh020917@gmail.com>
2026-04-20 08:38:31 +0530

    Added o(n) solution for Conatains Duplicate

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -1,5 +1,20 @@
 package com;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate {
+	
+	 public boolean containsDuplicate(int[] nums) {
+		 
+		 Set<Integer> set = new HashSet<>();
+		 
+		 for(int num:nums) {
+			 if(!set.add(num)) {  // add returns false if already present
+				 return true;
+			 }
+		 }
+		 return false;
+	 }
 
 }
